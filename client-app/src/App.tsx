7ca94de654c7a016c234aa1d293e4427f3c0591c
Леня Шубин <lenyashubin2002@gmail.com>
2024-01-25 21:39:21 +0200

    Fetched data from the API and added Semantic UI package

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivities(response.data)
+      })
+  }, [])
+
   return (
-    <h1>Reactivities</h1>
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>
+            {activity.title}
+          </List.Item>
+        ))}
+      </List>
+      
+    </div>
+    
   )
 }
 
